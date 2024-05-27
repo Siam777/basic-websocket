abe63f0241bcdf20f639aa89ef27d9702a88a2b0
siamriaz <siam.riaz@selise.ch>
2024-05-27 17:57:00 +0600

    initial commit

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { WebSocketServer } from "ws";
+
+const app = express();
+
+const port = 3000;
+
+const server = app.listen(port, () => {
+   console.log(`Server listening on port ${port}`);
+});
+
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", (ws) => {
+   ws.on("message", (data) => {
+      console.log("data from client %s:", data);
+      ws.send("Thanks");
+   })
+});
+
